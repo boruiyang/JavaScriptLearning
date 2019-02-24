af44de3726dd6f69757cf49cb71d1b610318b5c7
Borui Yang <boruiyang@hotmail.com>
2019-02-24 15:21:13 -0500

    Section 3 completed

--- Section 3/script.js
+++ Section 3/script.js
@@ -1,10 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+/*
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+};
+
+//retirement(1990);
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+};
+
+// variables
+
+console.log(age);
+var age = 23;
 
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
 
-
+foo();
+console.log(age);
+*/
 
 
 
@@ -58,7 +82,7 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log( a + d );
 }
 */
 
@@ -67,11 +91,38 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+/*
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth);
 
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+};
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
-
-
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
